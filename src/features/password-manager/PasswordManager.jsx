ae0ae06e640204a36b111dd40014e00de6bba70c
Samantha Colin <samantha.colin022@gmail.com>
2024-08-22 03:09:48 -0500

    update password manager ui

--- src/features/password-manager/PasswordManager.jsx
+++ src/features/password-manager/PasswordManager.jsx
@@ -34,7 +34,11 @@ function PasswordManager() {
     const [expandedItems, setExpandedItems] = useState({});
 
     const filteredPasswords = passwords.filter(pw =>
-        pw.name.toLowerCase().includes(searchTerm.toLowerCase())
+        pw.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        pw.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        pw.username?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        pw.email?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        pw.phone?.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const handleCopy = (text, field, pwId) => {
@@ -100,9 +104,17 @@ function PasswordManager() {
                         className="bg-white rounded-lg shadow-md p-4 mb-4 transition-all duration-300 ease-in-out hover:shadow-lg relative"
                     >
                         <div className="flex justify-between items-start mb-2">
-                            <div>
-                                <h3 className="font-bold text-lg text-gray-800">{pw.name}</h3>
-                                <p className="text-sm text-gray-600">{pw.description}</p>
+                            <div className="flex items-center">
+                                <h3 className="font-bold text-lg text-gray-800 mr-2">{pw.name}</h3>
+                                <Tooltip id={`expand-tooltip-${pw.id}`} />
+                                <button
+                                    data-tooltip-id={`expand-tooltip-${pw.id}`}
+                                    data-tooltip-content={expandedItems[pw.id] ? "See less" : "See all"}
+                                    onClick={() => toggleExpand(pw.id)}
+                                    className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
+                                >
+                                    {expandedItems[pw.id] ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
+                                </button>
                             </div>
                             <div className="flex items-center">
                                 {pw.url && (
@@ -118,14 +130,9 @@ function PasswordManager() {
                                         </button>
                                     </>
                                 )}
-                                <button
-                                    onClick={() => toggleExpand(pw.id)}
-                                    className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
-                                >
-                                    {expandedItems[pw.id] ? <ChevronUpIcon className="h-5 w-5" /> : <ChevronDownIcon className="h-5 w-5" />}
-                                </button>
                             </div>
                         </div>
+                        <p className="text-sm text-gray-600 mb-2">{pw.description}</p>
                         {editingId === pw.id ? (
                             <div className="flex-grow mr-4">
                                 <input
@@ -143,20 +150,22 @@ function PasswordManager() {
                                     className="w-full mb-2 p-2 border rounded shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                                     placeholder="Description"
                                 />
-                                <OptionalInput
-                                    name="email"
-                                    value={editPassword.email}
-                                    onChange={handleEditChange}
-                                    onClear={(name) => handleEditChange({target: {name, value: ''}})}
-                                    placeholder="Email (optional)"
-                                />
-                                <OptionalInput
-                                    name="username"
-                                    value={editPassword.username}
-                                    onChange={handleEditChange}
-                                    onClear={(name) => handleEditChange({target: {name, value: ''}})}
-                                    placeholder="Username (optional)"
-                                />
+                                <div className="grid grid-cols-2 gap-2 mb-2">
+                                    <OptionalInput
+                                        name="email"
+                                        value={editPassword.email}
+                                        onChange={handleEditChange}
+                                        onClear={(name) => handleEditChange({target: {name, value: ''}})}
+                                        placeholder="Email (optional)"
+                                    />
+                                    <OptionalInput
+                                        name="username"
+                                        value={editPassword.username}
+                                        onChange={handleEditChange}
+                                        onClear={(name) => handleEditChange({target: {name, value: ''}})}
+                                        placeholder="Username (optional)"
+                                    />
+                                </div>
                                 <OptionalInput
                                     name="phone"
                                     value={editPassword.phone}
@@ -191,9 +200,9 @@ function PasswordManager() {
                             </div>
                         ) : (
                             <div>
-                                <div className={`transition-all duration-300 ease-in-out ${expandedItems[pw.id] ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
+                                <div className="grid grid-cols-2 gap-2 mb-2">
                                     {pw.email && (
-                                        <p className="text-sm text-gray-700 flex items-center mb-2">
+                                        <p className="text-sm text-gray-700 flex items-center">
                                             Email: <span className="font-medium ml-2">{pw.email}</span>
                                             <Tooltip id={`email-tooltip-${pw.id}`}/>
                                             <button
@@ -209,7 +218,7 @@ function PasswordManager() {
                                         </p>
                                     )}
                                     {pw.username && (
-                                        <p className="text-sm text-gray-700 flex items-center mb-2">
+                                        <p className="text-sm text-gray-700 flex items-center">
                                             Username: <span className="font-medium ml-2">{pw.username}</span>
                                             <Tooltip id={`username-tooltip-${pw.id}`}/>
                                             <button
@@ -224,6 +233,35 @@ function PasswordManager() {
                                             </button>
                                         </p>
                                     )}
+                                </div>
+                                <p className="text-sm text-gray-700 flex items-center mb-2">
+                                    Password:
+                                    <span className="font-medium ml-2">
+                                        {showPassword[pw.id] ? pw.password : '••••••••'}
+                                    </span>
+                                    <Tooltip id={`toggle-tooltip-${pw.id}`}/>
+                                    <button
+                                        data-tooltip-id={`toggle-tooltip-${pw.id}`}
+                                        data-tooltip-content={showPassword[pw.id] ? "Hide password" : "Show password"}
+                                        onClick={() => toggleShowPassword(pw.id)}
+                                        className="ml-2 text-gray-400 hover:text-gray-600 transition-colors duration-200"
+                                    >
+                                        {showPassword[pw.id] ? <EyeSlashIcon className="h-5 w-5"/> :
+                                            <EyeIcon className="h-5 w-5"/>}
+                                    </button>
+                                    <Tooltip id={`password-tooltip-${pw.id}`}/>
+                                    <button
+                                        data-tooltip-id={`password-tooltip-${pw.id}`}
+                                        data-tooltip-content="Copy password"
+                                        onClick={() => handleCopy(pw.password, 'password', pw.id)}
+                                        className="ml-2 text-gray-400 hover:text-gray-600 transition-colors duration-200"
+                                    >
+                                        {copiedFields[pw.id]?.password ?
+                                            <ClipboardDocumentCheckIcon className="h-5 w-5"/> :
+                                            <ClipboardIcon className="h-5 w-5"/>}
+                                    </button>
+                                </p>
+                                <div className={`transition-all duration-300 ease-in-out ${expandedItems[pw.id] ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
                                     {pw.phone && (
                                         <p className="text-sm text-gray-700 flex items-center mb-2">
                                             Phone: <span className="font-medium ml-2">{pw.phone}</span>
@@ -240,33 +278,6 @@ function PasswordManager() {
                                             </button>
                                         </p>
                                     )}
-                                    <p className="text-sm text-gray-700 flex items-center mb-2">
-                                        Password:
-                                        <span className="font-medium ml-2">
-                                            {showPassword[pw.id] ? pw.password : '••••••••'}
-                                        </span>
-                                        <Tooltip id={`toggle-tooltip-${pw.id}`}/>
-                                        <button
-                                            data-tooltip-id={`toggle-tooltip-${pw.id}`}
-                                            data-tooltip-content={showPassword[pw.id] ? "Hide password" : "Show password"}
-                                            onClick={() => toggleShowPassword(pw.id)}
-                                            className="ml-2 text-gray-400 hover:text-gray-600 transition-colors duration-200"
-                                        >
-                                            {showPassword[pw.id] ? <EyeSlashIcon className="h-5 w-5"/> :
-                                                <EyeIcon className="h-5 w-5"/>}
-                                        </button>
-                                        <Tooltip id={`password-tooltip-${pw.id}`}/>
-                                        <button
-                                            data-tooltip-id={`password-tooltip-${pw.id}`}
-                                            data-tooltip-content="Copy password"
-                                            onClick={() => handleCopy(pw.password, 'password', pw.id)}
-                                            className="ml-2 text-gray-400 hover:text-gray-600 transition-colors duration-200"
-                                        >
-                                            {copiedFields[pw.id]?.password ?
-                                                <ClipboardDocumentCheckIcon className="h-5 w-5"/> :
-                                                <ClipboardIcon className="h-5 w-5"/>}
-                                        </button>
-                                    </p>
                                     {pw.notes && (
                                         <p className="text-sm text-gray-700 mb-2">
                                             Notes: <span className="font-medium">{pw.notes}</span>
@@ -324,7 +335,6 @@ function PasswordManager() {
                 ))}
             </AnimatePresence>
 
-            {/* Add Password Modal */}
             <AnimatePresence>
                 {showModal && (
                     <motion.div
@@ -447,7 +457,6 @@ function PasswordManager() {
                 )}
             </AnimatePresence>
 
-            {/* Add Password button */}
             <Tooltip id="add-password-tooltip" />
             <motion.button
                 whileHover={{ scale: 1.1 }}
